246178908360e649fce6cce3448dd252483ae16f
VinayAce <vinayreddy.ace@gmail.com>
2020-09-09 18:37:01 +0530

    Added other URL

--- InTableView/ViewController.swift
+++ InTableView/ViewController.swift
@@ -15,21 +15,34 @@ struct dataHolder {
     let email:String!
 }
 
+struct dataHolder1 {
+    let employee_name:String!
+    let employee_salary:Int!
+}
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var commentsTableView: UITableView!
     
-    var tableData:[dataHolder] = []
+   // var tableData:[dataHolder] = []
+    
+    var tableData:[dataHolder1] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        AF.request("https://jsonplaceholder.typicode.com/comments").responseJSON { (response) in
-           
+        let url = "https://jsonplaceholder.typicode.com/comments"
+        
+        let url1 = "http://dummy.restapiexample.com/api/v1/employees"
+        
+        AF.request(url1).responseJSON { (response) in
+            
             switch response.result {
+                
             case .success(let value):
-                //SwiftyJson
+                
+                //SwiftyJSON DH
 //               let json = JSON(value)
 //               for (_, object) in json {
 //                let name = object["name"].stringValue
@@ -41,25 +54,66 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 //                }
 //
 //               }
-                if let fullData = value as? [[String:Any]] {
-                    print(fullData, "Actual")
-                    fullData.forEach({ (key) in
-                        let name = key["name"] as? String
-                        let email = key["email"] as? String
-
-                        let dataHold = dataHolder(name: name, email: email)
-
-                        self.tableData.append(dataHold)
-                    })
-                    DispatchQueue.main.async {
-                        self.commentsTableView.reloadData()
-                    }
+                
+                   // SwiftyJSON DH1
+                let json = JSON(value)
+                for (_, object) in json["data"] {
+                let name = object["employee_name"].stringValue
+                let email = object["employee_salary"].int
+                let dataHold = dataHolder1(employee_name: name, employee_salary: email)
+                self.tableData.append(dataHold)
+                DispatchQueue.main.async {
+                    self.commentsTableView.reloadData()
                 }
+            }
+                
+                //My Parse DH
+                
+//                if let fullData = value as? [[String:Any]] {
+//                    print(fullData, "Actual")
+//                    fullData.forEach({ (key) in
+//                        let name = key["name"] as? String
+//                        let email = key["email"] as? String
+//
+//                        let dataHold = dataHolder(name: name, email: email)
+//
+//                        self.tableData.append(dataHold)
+//                    })
+//                    DispatchQueue.main.async {
+//                        self.commentsTableView.reloadData()
+//                    }
+//                }
+                
+                
+                //My Parse DH1
+                
+//                if let fullData = value as? [String:Any] {
+//                print(fullData, "Actual")
+//                if let dataModel = fullData["data"] as? [[String:Any]] {
+//
+//                    dataModel.forEach({ (key) in
+//                    let name = key["employee_name"] as? String
+//                    let salary = key["employee_salary"] as? Int
+//
+//                    let dataHold = dataHolder1(employee_name: name, employee_salary: salary)
+//
+//                    self.tableData.append(dataHold)
+//                })
+//
+//                    DispatchQueue.main.async {
+//
+//                    self.commentsTableView.reloadData()
+//
+//                    }
+//                }
+//            }
+                
             case .failure(let error):
                 print(error)
             }
-
-    }
+            
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,8 +127,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let allTableData = tableData[indexPath.row]
         
         DispatchQueue.main.async {
-        cell.title.text = allTableData.name
-        cell.descriptionComments.text = allTableData.email
+        cell.title.text = allTableData.employee_name //name
+        cell.descriptionComments.text = String(allTableData.employee_salary) //email
         }
         return cell
     }
