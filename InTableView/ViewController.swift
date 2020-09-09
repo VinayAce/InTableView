28fcd85b5780b227223cd4ac808290695eaf3435
VinayAce <vinayreddy.ace@gmail.com>
2020-09-09 14:48:57 +0530

    Desired output

--- InTableView/ViewController.swift
+++ InTableView/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+struct dataHolder {
+    let name:String!
+    let email:String!
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var commentsTableView: UITableView!
+    
+    var tableData:[dataHolder] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        AF.request("https://jsonplaceholder.typicode.com/comments").responseJSON { (response) in
+           
+            switch response.result {
+            case .success(let value):
+                if let fullData = value as? [[String:Any]] {
+                    print(fullData, "Actual")
+                    fullData.forEach({ (key) in
+                        let name = key["name"] as? String
+                        let email = key["email"] as? String
+                       
+                        let dataHold = dataHolder(name: name, email: email)
+                       
+                        self.tableData.append(dataHold)
+                    })
+                    DispatchQueue.main.async {
+                        self.commentsTableView.reloadData()
+                    }
+                }
+            case .failure(let error):
+                print(error)
+            }
+
     }
-
-
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:commentsTableViewCell = self.commentsTableView.dequeueReusableCell(withIdentifier: "commentsCell") as! commentsTableViewCell
+        
+        let allTableData = tableData[indexPath.row]
+        
+        DispatchQueue.main.async {
+        cell.title.text = allTableData.name
+        cell.descriptionComments.text = allTableData.email
+        }
+        return cell
+    }
+    
+    
+    
 }
 
+class commentsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var descriptionComments: UILabel!
+    
+}
---
+++ Pods/Target Support Files/Pods-InTableView/Pods-InTableView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InTableView : NSObject
+@end
+@implementation PodsDummy_Pods_InTableView
+@end
